e61ca4f26a2ccdc3ad2e62e2ada617640bb79e57
1shaked <shaked1hen@gmail.com>
2021-12-05 10:44:28 +0200

    Initial Commit

---
+++ ticTacToe/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  ticTacToe
+//
+//  Created by shaked chen on 05/12/2021.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
